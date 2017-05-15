9f792e8de909656f804573df1d1d8166d4d2728d
Thomas Sanchez <thomas.sanchz@gmail.com>
2017-05-15 19:17:36 +0200

    Support gcc 4.9

--- include/jni/string.hpp
+++ include/jni/string.hpp
@@ -5,8 +5,17 @@
 #include <jni/make.hpp>
 #include <jni/npe.hpp>
 
+#define JNI_HPP_LOCALE_BOOST 1
+#define JNI_HPP_LOCALE_CODECVT 2
+
 #include <locale>
-#include <codecvt>
+#if __has_include("codecvt")
+# include <codecvt>
+# define JNI_HPP_LOCALE JNI_HPP_LOCALE_CODECVT
+#else
+# include <boost/locale.hpp>
+# define JNI_HPP_LOCALE JNI_HPP_LOCALE_BOOST
+#endif
 
 namespace jni
    {
@@ -16,14 +25,6 @@ namespace jni
     struct UntaggedObjectType<StringTag> { using Type = jstring; };
 
     using String = Object<StringTag>;
-
-#if defined(_MSC_VER)
-    // Workaround for bug in VS2015/2017. See:
-    // https://connect.microsoft.com/VisualStudio/Feedback/Details/1403302
-    using Ch16 = std::u16string::traits_type::int_type;
-#else
-    using Ch16 = std::u16string::traits_type::char_type;
-#endif
 
     inline std::u16string MakeAnything(ThingToMake<std::u16string>, JNIEnv& env, const String& string)
        {
@@ -35,9 +36,12 @@ namespace jni
 
     inline std::string MakeAnything(ThingToMake<std::string>, JNIEnv& env, const String& string)
        {
-        std::u16string u16str = Make<std::u16string>(env, string);
-        return std::wstring_convert<std::codecvt_utf8_utf16<Ch16>, Ch16>()
-            .to_bytes(reinterpret_cast<const Ch16*>(u16str.data()));
+#if JNI_HPP_LOCALE == JNI_HPP_LOCALE_BOOST
+        return boost::locale::conv::utf_to_utf<char>(Make<std::u16string>(env, string));
+#elif JNI_HPP_LOCALE == JNI_HPP_LOCALE_CODECVT
+        return std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t>()
+            .to_bytes(Make<std::u16string>(env, string));
+#endif
        }
 
     inline String MakeAnything(ThingToMake<String>, JNIEnv& env, const std::u16string& string)
@@ -47,7 +51,11 @@ namespace jni
 
     inline String MakeAnything(ThingToMake<String>, JNIEnv& env, const std::string& string)
        {
-        auto wstr = std::wstring_convert<std::codecvt_utf8_utf16<Ch16>, Ch16>().from_bytes(string);
-        return Make<String>(env, reinterpret_cast<const char16_t*>(wstr.data()));
+#if JNI_HPP_LOCALE == JNI_HPP_LOCALE_BOOST
+        return Make<String>(env, boost::locale::conv::utf_to_utf<char16_t>(string));
+#elif JNI_HPP_LOCALE == JNI_HPP_LOCALE_CODECVT
+        return Make<String>(env, std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t>()
+            .from_bytes(string));
+#endif
        }
    }
